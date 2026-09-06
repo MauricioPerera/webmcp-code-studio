6aba59ce1864a7193eafcdff098dc16a5f991be9
Mauricio Perera <mauricio.perera@gmail.com>
2026-09-06 16:25:42 -0600

    feat: set educational landing page as index.html and route IDE to studio.html

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html',
-        landing: './landing.html',
+        studio: './studio.html',
       },
     },
   },
